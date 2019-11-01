93a99bd86b3ecf78415e7af6ff17fa2dee81b72e
Ubuntu <ubuntu@ip-172-31-85-111.ec2.internal>
2019-11-01 11:26:21 +0000

    security key replaced with environment variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5b5jx19j!2!*df*ps7z-8&q98g$ttg5%ald(1yofp$fusx&qo*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development # if development is True then debug is True is False, then Debug is also False {don't have to change it back for Production
